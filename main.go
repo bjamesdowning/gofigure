d9d10de7c4bb0975322e33fdbb9bf2060044b38b
bjamesdowning <bjamesdowning@gmail.com>
2018-06-05 02:59:08 +0300

    first commit

--- main.go
+++ main.go
@@ -1,36 +1,203 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
-	"os"
 
-	"github.com/bjamesdowning/web2mongo/controllers"
-	"github.com/julienschmidt/httprouter"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/satori/go.uuid"
 )
 
-//basic echo web server. Allows environment variable PORT to dicate listening port.
-//user 'export PORT=<port> to set
+type user struct {
+	Email string
+	Fname string
+	Lname string
+	Role  string
+	Pword []byte
+}
+
+var tmpl *template.Template
+var dbSessions = map[string]string{}
+var dbUsers = map[string]user{}
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("templates/*"))
+}
+
 func main() {
-	mux := httprouter.New()
-	mux.GET("/", index)
-	mux.GET("/api/echo", controllers.Echo)
-	mux.POST("/api/books", controllers.BooksHandler)
-	mux.POST("/api/books/", controllers.BookHandler)
-	http.ListenAndServe(port(), mux)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/signup", signUp)
+	http.HandleFunc("/adminhome", adminHome)
+	http.HandleFunc("/userhome", userHome)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
 }
 
-//dynamic listening port
-func port() string {
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = ":8080"
+func index(w http.ResponseWriter, r *http.Request) {
+	u := getUser(w, r)
+	tmpl.ExecuteTemplate(w, "index.html", u)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if loggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
-	return ":" + port
+	if r.Method == http.MethodPost {
+		email := r.FormValue("email")
+		pword := r.FormValue("password")
+
+		u, ok := dbUsers[email]
+		if !ok {
+			http.Error(w, "Username/Password Error", http.StatusForbidden)
+			return
+		}
+		err := bcrypt.CompareHashAndPassword(u.Pword, []byte(pword))
+		if err != nil {
+			http.Error(w, "Username/Password Error", http.StatusForbidden)
+			return
+		}
+		//Username and Password match. Create a session
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = email
+
+		if u.Role == "admin" {
+			http.Redirect(w, r, "/adminhome", http.StatusSeeOther)
+			return
+		} else if u.Role == "user" {
+			http.Redirect(w, r, "/userhome", http.StatusSeeOther)
+			return
+		}
+	}
+	tmpl.ExecuteTemplate(w, "login.html", nil)
 }
 
-//responds with http code 200 and message
-func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Cloud native go course on %s.", os.Getenv("PORT"))
+func logout(w http.ResponseWriter, r *http.Request) {
+	if !loggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	c, _ := r.Cookie("session")
+	//remove session from db
+	delete(dbSessions, c.Value)
+	//delete cookie by assigning new cookie with negative age value
+	c = &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, c)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func signUp(w http.ResponseWriter, r *http.Request) {
+	if loggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	var u user
+	//process form to create user
+	if r.Method == http.MethodPost {
+		email := r.FormValue("email")
+		fname := r.FormValue("firstname")
+		lname := r.FormValue("lastname")
+		role := r.FormValue("role")
+		pword := r.FormValue("password")
+		//Check if username is already in the db
+		if _, ok := dbUsers[email]; ok {
+			http.Error(w, "Username Taken", http.StatusForbidden)
+			return
+		}
+		//Create a session
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = email
+		//Store user info. First, encrpt pword
+		bs, err := bcrypt.GenerateFromPassword([]byte(pword), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Server Error, Password Related", http.StatusInternalServerError)
+			return
+		}
+		u = user{email, fname, lname, role, bs}
+		dbSessions[c.Value] = email
+		dbUsers[email] = u
+		if u.Role == "admin" {
+			http.Redirect(w, r, "/adminhome", http.StatusSeeOther)
+			return
+		} else if u.Role == "user" {
+			http.Redirect(w, r, "/userhome", http.StatusSeeOther)
+			return
+		}
+	}
+
+	tmpl.ExecuteTemplate(w, "signup.html", u)
+
+}
+
+func adminHome(w http.ResponseWriter, r *http.Request) {
+	u := getUser(w, r)
+	if !loggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	if u.Role != "admin" {
+		http.Error(w, "Only Admins Allowed", http.StatusForbidden)
+		return
+	}
+	tmpl.ExecuteTemplate(w, "admin.html", u)
+}
+
+func userHome(w http.ResponseWriter, r *http.Request) {
+	u := getUser(w, r)
+	if !loggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	if u.Role != "user" {
+		http.Error(w, "Users Homepage", http.StatusForbidden)
+		return
+	}
+	tmpl.ExecuteTemplate(w, "user.html", u)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) user {
+	//Check for cookie
+	c, err := r.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4() //throw away error.
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	//At this point, the user has a cookie
+	var u user
+	if email, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[email]
+	}
+	return u
+}
+
+func loggedIn(r *http.Request) bool {
+	c, err := r.Cookie("session")
+	if err != nil {
+		return false
+	}
+	email := dbSessions[c.Value]
+	_, ok := dbUsers[email]
+	return ok
 }
